e7c03c35ec17d87b622afde467c0cf0fe4f2aed9
Ashton Fagg <ashton@fagg.id.au>
2024-10-26 23:00:47 -0400

    Slim down the exes by dynamically linking and some other shit

--- build.bat
+++ build.bat
@@ -2,11 +2,11 @@
 
 if not exist .\bin mkdir .\bin
 
-cl.exe monoff.cpp /nologo /EHsc /Fe:bin\monoff.exe /link shell32.lib user32.lib ole32.lib oleaut32.lib
+cl.exe monoff.cpp /nologo /EHsc /Os /MD /DNDEBUG /Fe:bin\monoff.exe /link /OPT:REF /OPT:ICF shell32.lib user32.lib ole32.lib oleaut32.lib
 del monoff.obj
 
-cl.exe touch.cpp /nologo /EHsc /Fe:bin\touch.exe /link shell32.lib user32.lib ole32.lib oleaut32.lib
+cl.exe touch.cpp /nologo /EHsc /Os /MD /DNDEBUG /Fe:bin\touch.exe /link /OPT:REF /OPT:ICF shell32.lib user32.lib ole32.lib oleaut32.lib
 del touch.obj
 
-cl.exe pwd.cpp /nologo /EHsc /Fe:bin\pwd.exe /link shell32.lib user32.lib ole32.lib oleaut32.lib
+cl.exe pwd.cpp /nologo /EHsc /Fe:bin\pwd.exe /MD /DNDEBUG /link /OPT:REF /OPT:ICF shell32.lib user32.lib ole32.lib oleaut32.lib
 del pwd.obj
